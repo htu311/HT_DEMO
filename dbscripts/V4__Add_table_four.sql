0194ea3815adf25cd947781d02af9e8968b18b9e
Dipesh <dipesh.devlekar@hoonartek.com>
2023-01-24 13:54:00 +0000

    Create V4__Add_table_four.sql

---
+++ dbscripts/V4__Add_table_four.sql
@@ -0,0 +1,11 @@
+use database pipeline_db_migration_plan;
+use schema my_schema;
+
+CREATE TABLE table_four (
+  value VARCHAR(25) NOT NULL PRIMARY KEY
+);
+
+
+CREATE TABLE table_five (
+  value VARCHAR(25) NOT NULL PRIMARY KEY
+);
